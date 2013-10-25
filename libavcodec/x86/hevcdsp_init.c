be865f89701376e74b27ba0f53204251fc252f31
wassim Hamidouche <whamidouch@gmail.com>
2013-10-25 18:40:58 +0200

    add sse up filters

--- libavcodec/x86/hevcdsp_init.c
+++ libavcodec/x86/hevcdsp_init.c
@@ -130,6 +130,9 @@ void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth)
                     c->put_hevc_qpel[3][1] = ff_hevc_put_hevc_qpel_h_1_v_3_sse;
                     c->put_hevc_qpel[3][2] = ff_hevc_put_hevc_qpel_h_2_v_3_sse;
                     c->put_hevc_qpel[3][3] = ff_hevc_put_hevc_qpel_h_3_v_3_sse;
+                    c->upsample_base_layer_frame = ff_upsample_base_layer_frame_sse;
+                    c->upsample_h_base_layer_frame = ff_upsample_base_layer_frame_sse_h;
+                    c->upsample_v_base_layer_frame = ff_upsample_base_layer_frame_sse_v;
 
 
                 }
